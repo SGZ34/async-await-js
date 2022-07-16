b512bc67caa94669427180eada34d96aa19786f0
SGZ34 <simongiraldo34@gmail.com>
2022-07-16 15:34:33 -0500

    first commit and add files

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-// fetch("https://jsonplaceholder.typicode.com/todos")
-//   .then((res) => res.json())
-//   .then((respuesta) => {
-//     respuesta.forEach((element) => {
-//       console.log(element);
-//     });
-//   });
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json())
+  .then((respuesta) => {
+    respuesta.forEach((element) => {
+      console.log(element);
+    });
+  });
 
-// axios.get("https://jsonplaceholder.typicode.com/todos").then(({ data }) =>
-//   data.forEach((element) => {
-//     console.log(element);
-//   })
-// );
+axios.get("https://jsonplaceholder.typicode.com/todos").then(({ data }) =>
+  data.forEach((element) => {
+    console.log(element);
+  })
+);
 
-// const obtenerDatosFetch = async () => {
-//   const respuesta = await fetch(
-//     "https://jsonplaceholder.typicode.com/todos"
-//   ).then((res) => res.json());
-//   respuesta.forEach((element) => {
-//     console.log(element);
-//   });
-// };
+const obtenerDatosFetch = async () => {
+  const respuesta = await fetch(
+    "https://jsonplaceholder.typicode.com/todos"
+  ).then((res) => res.json());
+  respuesta.forEach((element) => {
+    console.log(element);
+  });
+};
 
-// obtenerDatosFetch();
+obtenerDatosFetch();
 
-// const obtenerDatosAxios = async () => {
-//   const { data } = await axios.get(
-//     "https://jsonplaceholder.typicode.com/todos"
-//   );
-//   data.forEach((element) => {
-//     console.log(element);
-//   });
-// };
+const obtenerDatosAxios = async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+  data.forEach((element) => {
+    console.log(element);
+  });
+};
 
-// obtenerDatosAxios();
-
+obtenerDatosAxios();
