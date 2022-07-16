6a90b51131c33b9c6dbfe76982d5f7be14f85756
SGZ34 <simongiraldo34@gmail.com>
2022-07-16 15:17:51 -0500

    first commit and add files

---
+++ main.js
@@ -0,0 +1,36 @@
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((respuesta) => {
+//     respuesta.forEach((element) => {
+//       console.log(element);
+//     });
+//   });
+
+// axios.get("https://jsonplaceholder.typicode.com/todos").then(({ data }) =>
+//   data.forEach((element) => {
+//     console.log(element);
+//   })
+// );
+
+// const obtenerDatosFetch = async () => {
+//   const respuesta = await fetch(
+//     "https://jsonplaceholder.typicode.com/todos"
+//   ).then((res) => res.json());
+//   respuesta.forEach((element) => {
+//     console.log(element);
+//   });
+// };
+
+// obtenerDatosFetch();
+
+// const obtenerDatosAxios = async () => {
+//   const { data } = await axios.get(
+//     "https://jsonplaceholder.typicode.com/todos"
+//   );
+//   data.forEach((element) => {
+//     console.log(element);
+//   });
+// };
+
+// obtenerDatosAxios();
+
